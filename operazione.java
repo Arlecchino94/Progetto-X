c71584ae335f323f1a9847851049029f31f5a2e0
Khali <vetralla.alberto@hotmail.it>
2024-03-20 14:42:36 +0100

    x

--- operazione.java
+++ operazione.java
@@ -3,24 +3,31 @@ import javax.swing.*;
 public class operazione {
     public static void op(String s, int n, int m) {
         double risultato = 0;
+        boolean op = true;
         String output = "";
-        if (s.equals("*")) {
-            risultato = n * m;
-            output+= risultato;
+        if (!s.equals("*") && !s.equals("/") && !s.equals("+") && !s.equals("-")) {
+            output += "Il segno è errato";
+            return;
         }
-        if (s.equals("/")) {
-            risultato = (double) n / m;
-            output += risultato;
-        }
-        if (s.equals("-")) {
-            risultato = n-m;
-            output += risultato;
-        }
-        if (s.equals("+")){
-            risultato += n+m;
-            output += risultato;
+        if (op == true) {
+           if (s.equals("*")) {
+                risultato = n * m;
+                output += risultato;
+            }
+           if (s.equals("/")) {
+                risultato = (double) n / m;
+                output += risultato;
+            }
+           if (s.equals("-")) {
+                risultato = n - m;
+                output += risultato;
+            }
+           if (s.equals("+")) {
+                risultato += n + m;
+                output += risultato;
+            }
         }
         JOptionPane.showMessageDialog(null, output);
-
     }
 }
+
