ef5441b3ff09e02a3dcda4010ba41c4287c3c7d3
Khali <vetralla.alberto@hotmail.it>
2024-03-20 18:33:03 +0100

    x

--- operazione.java
+++ operazione.java
@@ -4,30 +4,29 @@ public class operazione {
     public static void op(String s, int n, int m) {
         double risultato = 0;
         String output = "";
-            switch (s) {
-                case "*":
-                    risultato = n * m;
-                    output += risultato;
-                    break;
-                case "/":
-                    risultato = (double) n / m;
-                    output +=  risultato;
-                    break;
-                case "+":
-                    risultato = n + m;
-                    output += risultato;
-                    break;
-                case "-":
-                    risultato = n - m;
-                    output += risultato;
-                    break;
+        switch (s) {
+            case "*":
+                risultato = n * m;
+                output += risultato;
+                break;
+            case "/":
+                risultato = (double) n / m;
+                output += risultato;
+                break;
+            case "+":
+                risultato = n + m;
+                output += risultato;
+                break;
+            case "-":
+                risultato = n - m;
+                output += risultato;
+                break;
 
-                default:
-                    output += "Il segno è errato";
-                    break;
-            }
-            JOptionPane.showMessageDialog(null, output);
+            default:
+                output += "Il segno è errato";
+                break;
         }
+        JOptionPane.showMessageDialog(null, output);
     }
 }
 
