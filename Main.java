fcde6a0e9f695f6c17126fd47d371a3eb0681203
Khali <vetralla.alberto@hotmail.it>
2024-03-20 18:39:19 +0100

    x

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         int n = Lettura.Lettura("Inserire il primo numero");
-        String s = Lettura.Segno("Dimmi se vuoi sommare, dividere, moltiplicare o fare la differenza");
+        String s = "";
+        do {
+            s = Lettura.Segno("Dimmi se vuoi sommare, dividere, moltiplicare o fare la differenza");
+        }
+        while (!s.equals("*") && !s.equals("+") && !s.equals("/") && !s.equals("-"));
         int m = Lettura.Lettura("Inserire il secondo numero");
         operazione.op(s,n,m);
     }
